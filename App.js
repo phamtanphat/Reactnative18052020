ecde24ea56092416b3f4aae2c8e683326c49cae5
phamtanphat <phatdroid94@gmail.com>
2020-06-08 05:43:51 -0700

    bai tap 1

--- App.js
+++ App.js
@@ -1,33 +1,78 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View , StyleSheet} from 'react-native'
 
+// Flex box
 class App extends Component {
   render() {
     return (
-      <View style={{
-        marginTop : 50 ,
-        backgroundColor : 'darkseagreen', 
-        borderRadius: 5 , 
-        marginHorizontal : 10,
-        padding: 20,
-        borderColor : 'black',
-        borderWidth : 1,
-      }}>
-        <Text
-          style={{
-            fontSize : 20 , 
-            fontWeight : '300' ,
-            textAlign: 'center',
-            width : 200,
-            height: 300,
-            backgroundColor: 'red',
-        }}>
-          Name : Pham Tan Phat
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.containerVertical}>
+          <View style={styles.wrapperText1}>
+            <Text style={styles.textStyleVertical}>A</Text>
+          </View>
+          <View style={styles.wrapperText1}>
+            <Text style={styles.textStyleVertical}>B</Text>
+          </View> 
+          <View style={styles.wrapperText1}>
+            <Text style={styles.textStyleVertical}>C</Text>
+          </View> 
+          <View style={styles.wrapperText1}>
+            <Text style={styles.textStyleVertical}>D</Text>
+          </View>
+        </View>
+        <View style={styles.containerHorizontal}>
+        <View style={styles.wrapperText2}>
+            <Text style={styles.textStyleVertical}>A</Text>
+          </View>
+          <View style={styles.wrapperText2}>
+            <Text style={styles.textStyleVertical}>B</Text>
+          </View> 
+          <View style={styles.wrapperText2}>
+            <Text style={styles.textStyleVertical}>C</Text>
+          </View> 
+          <View style={styles.wrapperText2}>
+            <Text style={styles.textStyleVertical}>D</Text>
+          </View>
+        </View>
       </View>
     )
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    marginTop : 50 ,
+    marginBottom: 25,
+    flex: 1,
+    flexDirection : 'column'
+  },
+  containerVertical : {
+    flex : 1,
+    backgroundColor : 'red',
+    flexDirection : 'column'
+  },
+  wrapperText1 : {
+    flex: 1,
+    borderWidth : 1 ,
+    alignItems: 'center',
+    justifyContent : 'center',
+    backgroundColor : 'blue'
+  },
+  textStyleVertical: {
+  },
+  containerHorizontal: {
+    flex : 1 ,
+    backgroundColor: 'yellow',
+    flexDirection : 'row'
+  },
+  wrapperText2 : {
+    flex: 1,
+    borderWidth : 1 ,
+    borderColor :'black',
+    alignItems: 'center',
+    justifyContent : 'center',
+    backgroundColor : 'orange'
+  },
+})
 
 export default App;
 
