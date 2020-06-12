bc794b3ce737671ec2dd8559d967f58e2382c9b8
phamtanphat <phatdroid94@gmail.com>
2020-06-12 04:03:52 -0700

    state

--- src/components/Box.js
+++ src/components/Box.js
@@ -8,25 +8,36 @@ export default class Box extends Component {
             count : 1
         }
     }
-
+    increase(){
+        this.setState({count : this.state.count + 1})
+    }
+    descrease(){
+        this.setState({count : this.state.count - 1})
+    }
+    reset(){
+        this.setState({count : 0})
+    }
     // khi udapte gia tri : nen tao ra 1 gia tri moi , khong nen xu ly truc tiep cho gia tri
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.textCount}> Count :  {this.state.count}</Text>
+                <Text style={styles.textCount}> 
+                    Count :  {this.state.count}
+                </Text>
                 <View style={styles.eventgroup}>
                     <TouchableOpacity 
-                        onPress={() => {
-                            this.setState({count : this.state.count++})
-                            console.log(this.state)
-                        }}
+                        onPress={() => this.increase()}
                         style={styles.boxIncrease}>
                         <Text style={styles.increase}>InCrease</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boxDescrease}>
+                    <TouchableOpacity 
+                        onPress={() => this.descrease()}
+                        style={styles.boxDescrease}>
                         <Text style={styles.descrease}>DesCrease</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.boxReset}>
+                    <TouchableOpacity 
+                        onPress={() => this.reset()}
+                        style={styles.boxReset}>
                         <Text style={styles.reset}>Reset</Text>
                     </TouchableOpacity>
                 </View>
